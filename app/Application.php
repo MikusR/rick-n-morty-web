948ef54dac603e3471eb45695b78de1d2d2231a0
MikusR <mikus.rakis@gmail.com>
2023-11-03 11:39:26 +0200

    init

---
+++ app/Application.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+class Application
+{
+
+    public function run()
+    {
+        echo 'Hullo';
+    }
+
+    public function __construct()
+    {
+        echo 'cccc';
+    }
+}
